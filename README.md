ff5242a98b5dc4bdc45fec593e1daeba34af2b81
tr13ze <13cryss@gmail.com>
2016-05-25 00:15:22 +0200

    first

---
+++ README.md
@@ -0,0 +1,14 @@
+Installation
+    $ npm install
+
+Usage
+    $ npm run build
+
+To build on production mode
+    $ NODE_ENV=production npm run build
+
+To use webpack dev server & watch,
+    $ npm start
+
+
+Listens at http://localhost:8080/www/
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var React = require('react')
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+function getDevTool() {
+    if (process.env.NODE_ENV !== 'production') {
+        return 'source-map'; //enables source map
+    }
+
+    return false;
+}
+
+module.exports = {
+    entry: {
+        main: './src/js/main.js'
+    },
+    output: {
+        filename: './www/js/app.min.js'
+    },
+    devtool: getDevTool(),
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'react-hot'
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('css!sass')
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('www/css/app.min.css', { allChunks: true }),
+    ]
+
+};
